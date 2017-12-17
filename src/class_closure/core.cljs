e411165606cff7ae02d4ad674137190561f96e06
Cliff Rodgers <kliph@users.noreply.github.com>
2017-12-16 21:38:30 -0500

    Mimic plot from class closures blog post
    - Could scale x axis as a time axis, but that may introduce noise due
      to infrequent commits
      - May be more meaningful or projects under more consistent
      development
    
    - Hard code data for now

--- src/class_closure/core.cljs
+++ src/class_closure/core.cljs
@@ -1,21 +1,57 @@
 (ns class-closure.core
   (:require [reagent.core :as r]
+            [class-closure.state :as state]
             [cljsjs.chartjs]
-            [goog.dom]))
+            [goog.dom]
+            [goog.date]))
 
 (def by-id goog.dom.getElement)
 
 (defn render-chart []
   (let [context (-> (by-id "chart")
                     (.getContext "2d"))
-        chart-data {:type "bar"
-                    :data {:labels ["2012" "2013" "2014" "2015" "2016"]
-                           :datasets [{:data [5 10 15 20 25]
-                                       :label "Rev in MM"
-                                       :backgroundColor "#90EE90"}
-                                      {:data [3 6 9 12 15]
-                                       :label "Cost in MM"
-                                       :backgroundColor "#F08080"}]}}]
+        dates-csv (@state/app-state :dates)
+        dates (->> (clojure.string/split dates-csv "\n")
+                   (drop 1)
+                   (map #(let [[commit date] (clojure.string/split % ",")]
+                           [(keyword commit) date]))
+                   (into {}))
+        closures-csv (@state/app-state :closures)
+        closure-triplets (->> (clojure.string/split closures-csv "\n")
+                              (drop 1)
+                              (map #(let [[filename first-commit last-commit] (clojure.string/split % ",")]
+                                      [filename (get dates (keyword first-commit)) (get dates (keyword last-commit))])))
+        first-commits (->> (map (fn [[filename first-commit _]]
+                                  [filename first-commit]) closure-triplets)
+                           (into {}))
+        last-commits (->> (map (fn [[filename _ last-commit]]
+                                 [filename last-commit]) closure-triplets)
+                          (into {}))
+        commit-dates (->> (concat (vals first-commits) (vals last-commits))
+                          (into #{})
+                          (into [])
+                          sort)
+        d (nth commit-dates 3)
+        get-counts (fn [dataset]
+                     (->> (map (fn [d]
+                                 (-> (filter (fn [[k v]] (= d v)) dataset)
+                                     count))
+                               commit-dates)
+                          (reductions +)))
+        first-counts (get-counts first-commits)
+        last-counts (get-counts last-commits)
+        banal-blue "#2f588b"
+        ugly-green "#5d9649"
+        chart-data {:type "line"
+                    :data {:labels commit-dates
+                           :datasets [{:data first-counts
+                                       :label "Open Files"
+                                       :lineTension 0
+                                       :borderColor banal-blue}
+                                      {:data last-counts
+                                       :label "Closed Files"
+                                       :lineTension 0
+                                       :borderColor ugly-green}]}}]
     (js/Chart. context (clj->js chart-data))))
 
 (defn app-container []
